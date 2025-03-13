9572ab0043df6acdc4504599f7a2a052d49601ba
Morong Radek <morra@ton.cz>
2025-03-13 22:26:59 +0100

    bug: relative img pos

--- src/components/ui/second-page-ui/offer-div.tsx
+++ src/components/ui/second-page-ui/offer-div.tsx
@@ -6,24 +6,21 @@ import { FaChrome } from "react-icons/fa";
 export const OfferDiv = () => {
   return (
     <div className="text-base w-full group self-center lg:self-start max-w-[700px] flex flex-col items-center">
-      <div className="flex flex-col gap-10 w-full justify-between">
+      <div className="flex flex-col w-full justify-between">
+        <div className="h-[1px] z-20 flex self-end w-[130px] relative">
+          <img
+            loading="lazy"
+            src="/vector/sit-border-light.png"
+            alt="ikona programátora"
+            className="block dark:hidden h-[300px] right-20 top-1/2 -translate-y-1/2"
+          />
+        </div>
         <div className="w-full py-10 text-5xl relative z-0 flex flex-col text-start drop-shadow-md shadow-gray-200 shadow-lg bg-white border-[1px] dark:shadow-none dark:border-slate-700/50 p-4 rounded-xl border-gray-300 dark:bg-slate-900">
           <FaChrome className="text-green-700 dark:text-gray-500 self-start h-10 w-10 mb-2" />
           <H3Title title={offerText[0].title} />
           <p className="text-sm text-start mt-1 flex self-start w-full">
             {offerText[0].text}
           </p>
-
-          <div className="absolute top-0 right-20">
-            <div className="relative w-[130px] h-1">
-              <img
-                loading="lazy"
-                src="/vector/sit-border-light.png"
-                alt="ikona programátora"
-                className="block dark:hidden top-1/2 -translate-y-1/2"
-              />
-            </div>
-          </div>
         </div>
       </div>
     </div>
