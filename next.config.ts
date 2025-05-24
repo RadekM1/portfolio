9aec3a91615743456c145bba4a9e55664c5b2d43
Morong Radek <morra@ton.cz>
2025-05-24 21:54:08 +0200

    feat: netlify test

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ const nextConfig: NextConfig = {
   output: "export",
   trailingSlash: true,
   basePath: "",
-  assetPrefix: "",
+  assetPrefix: "https://radekmorong.cz/",
 };
 
 export default nextConfig;
